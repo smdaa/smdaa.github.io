6ebac32de82155b95f6057a33574eb1643c74652
smdaa <saadmdaa03@gmail.com>
2024-12-22 11:16:52 +0100

    migrate to astro

--- astro.config.ts
+++ astro.config.ts
@@ -47,6 +47,10 @@ export default defineConfig({
     contentLayer: true,
   },
   redirects: {
-    '/cinder-experiments/rendering-julia-set-fractal/main.html': '/posts/rendering-julia-set-fractal/'
+    '/cinder-experiments/rendering-julia-set-fractal/main.html': '/posts/rendering-julia-set-fractal/',
+    '/random-blogs/building-an-autograd-library-from-scratch-in-c-for-simple-neural-networks/main.html': '/posts/building-an-autograd-library-from-scratch-in-c-for-simple-neural-networks/',
+    '/random-blogs/optimizing-cpu-matrix-multiplication/main.html': '/posts/optimizing-cpu-matrix-multiplication/',
+    '/cinder-experiments/simulating_fluid/main.html': '/posts/simulating-fluid/',
+    '/cinder-experiments/building_a_dynamic_particle_system/main.html': '/posts/building-a-dynamic-particle-system/',
   }
 });
